5621ae9cb69c3572fffaa7754fde9fb740a960a3
aladly.forever@gmail.com <aladly.forever@gmail.com@039ad29e-8322-2af2-6ac3-2429411587d5>
2011-12-10 00:02:15 +0000

    - Fix the problems caused by the Timer class by replacing it with normal threads.

--- IntergalacticPhotoshop GUI/IntergalacticControls/Classes/UIHelpers.cs
+++ IntergalacticPhotoshop GUI/IntergalacticControls/Classes/UIHelpers.cs
@@ -20,7 +20,7 @@
     /// <summary>
     /// Used to pass parameters to a thread start, to remove elements from a panel.
     /// </summary>
-    struct RemoveElementParameters
+    struct DelayActionParameters
     {
         /// <summary>
         /// The element to remove
@@ -38,16 +38,66 @@
         private double delayInSeconds;
 
         /// <summary>
-        /// Initializes a new instance of the RemoveElementParameters struct
+        /// The dispatcher to invoke actions to
+        /// </summary>
+        private Dispatcher dispatcher;
+
+        /// <summary>
+        /// The action to perform after delay
+        /// </summary>
+        private Delegate function;
+
+        /// <summary>
+        /// List of action parameter
+        /// </summary>
+        private object[] parameters;
+
+        /// <summary>
+        /// Initializes a new instance of the DelayActionParameters struct
         /// </summary>
         /// <param name="element">The element to remove</param>
         /// <param name="panel">The parent panel</param>
         /// <param name="delayInSeconds">Time before removal</param>
-        public RemoveElementParameters(FrameworkElement element, Panel panel, double delayInSeconds)
+        public DelayActionParameters(FrameworkElement element, Panel panel, double delayInSeconds)
         {
             this.element = element;
             this.panel = panel;
             this.delayInSeconds = delayInSeconds;
+            this.dispatcher = null;
+            this.function = null;
+            this.parameters = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DelayActionParameters struct
+        /// </summary>
+        /// <param name="element">The element to remove</param>
+        /// <param name="delayInSeconds">Time before removal</param>
+        public DelayActionParameters(FrameworkElement element, double delayInSeconds)
+        {
+            this.element = element;
+            this.delayInSeconds = delayInSeconds;
+            this.panel = null;
+            this.dispatcher = null;
+            this.function = null;
+            this.parameters = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DelayActionParameters struct
+        /// </summary>
+        /// <param name="dispatcher">The dispatcher to invoke to</param>
+        /// <param name="function">The action to perform</param>
+        /// <param name="delayInSeconds">Time before the action call</param>
+        /// <param name="parameters">Parameters list</param>
+        public DelayActionParameters(Dispatcher dispatcher, Delegate function, double delayInSeconds, object[] parameters)
+        {
+            this.delayInSeconds = delayInSeconds;
+            this.dispatcher = dispatcher;
+            this.function = function;
+            this.parameters = parameters;
+            this.panel = null;
+            this.element = null;
         }
 
         /// <summary>
@@ -72,6 +122,30 @@
         public double DelayInSeconds
         {
             get { return this.delayInSeconds; }
+        }
+
+        /// <summary>
+        /// Gets the dispatcher to use for invoking actions
+        /// </summary>
+        public Dispatcher Dispatcher
+        {
+            get { return this.dispatcher; }
+        }
+
+        /// <summary>
+        /// Gets the dispatcher to use for invoking actions
+        /// </summary>
+        public Delegate Function
+        {
+            get { return this.function; }
+        }
+
+        /// <summary>
+        /// Gets the list of parameters to give to the action
+        /// </summary>
+        public object[] Parameters
+        {
+            get { return this.parameters; }
         }
     }
 
@@ -248,7 +322,7 @@
         /// <param name="delayInSeconds">Delay time in seconds</param>
         public static void RemoveElementFromContainerAfterDelay(FrameworkElement element, Panel panel, double delayInSeconds)
         {
-            RemoveElementParameters parameters = new RemoveElementParameters(element, panel, delayInSeconds);
+            DelayActionParameters parameters = new DelayActionParameters(element, panel, delayInSeconds);
             Thread thread = new Thread(new ParameterizedThreadStart(new ParameterizedThreadStart(RemoveElementThreadStart)));
             thread.Start(parameters);
         }
@@ -260,21 +334,9 @@
         /// <param name="delayInSeconds">Delay time in seconds</param>
         public static void HideElementAfterDelay(FrameworkElement element, double delayInSeconds)
         {
-            Timer timer = new Timer(
-                obj =>
-                {
-                    if (element.Dispatcher.Thread == Thread.CurrentThread)
-                    {
-                        HideElementHelper(element);
-                    }
-                    else
-                    {
-                        element.Dispatcher.BeginInvoke(new HideElementDelegate(HideElementHelper), element).Wait();
-                    }
-                },
-            null,
-            (int)(delayInSeconds * 1000),
-            Timeout.Infinite);
+            DelayActionParameters parameters = new DelayActionParameters(element, delayInSeconds);
+            Thread thread = new Thread(new ParameterizedThreadStart(new ParameterizedThreadStart(HideElementThreadStart)));
+            thread.Start(parameters);
         }
 
         /// <summary>
@@ -284,97 +346,11 @@
         /// <param name="dispatcher">The dispatcher object</param>
         /// <param name="function">The function to call</param>
         /// <param name="parameters">Function parameters</param>
-        public static void CallFunctionAfterDelay(double delayInSeconds, Dispatcher dispatcher, Delegate function, params object[] parameters)
+        public static void CallFunctionAfterDelay(double delayInSeconds, Dispatcher dispatcher, Delegate function, params object[] parametersList)
         {
-            Timer timer = new Timer(
-                obj =>
-                {
-                    if (dispatcher.Thread == Thread.CurrentThread)
-                    {
-                        switch (parameters.Length)
-                        {
-                            case 0:
-                                function.DynamicInvoke();
-                                break;
-                            case 1:
-                                function.DynamicInvoke(parameters[0]);
-                                break;
-                            case 2:
-                                function.DynamicInvoke(parameters[0], parameters[1]);
-                                break;
-                            case 3:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2]);
-                                break;
-                            case 4:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3]);
-                                break;
-                            case 5:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]);
-                                break;
-                            case 6:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5]);
-                                break;
-                            case 7:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6]);
-                                break;
-                            case 8:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[1]);
-                                break;
-                            case 9:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[7], parameters[8]);
-                                break;
-                            case 10:
-                                function.DynamicInvoke(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[7], parameters[8], parameters[9]);
-                                break;
-                            default:
-                                throw new InvalidOperationException("Parameters are too much.");
-                        }
-                    }
-                    else
-                    {
-                        switch (parameters.Length)
-                        {
-                            case 0:
-                                dispatcher.BeginInvoke(function).Wait();
-                                break;
-                            case 1:
-                                dispatcher.BeginInvoke(function, parameters[0]).Wait();
-                                break;
-                            case 2:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1]).Wait();
-                                break;
-                            case 3:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2]).Wait();
-                                break;
-                            case 4:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3]).Wait();
-                                break;
-                            case 5:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]).Wait();
-                                break;
-                            case 6:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5]).Wait();
-                                break;
-                            case 7:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6]).Wait();
-                                break;
-                            case 8:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[1]).Wait();
-                                break;
-                            case 9:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[7], parameters[8]).Wait();
-                                break;
-                            case 10:
-                                dispatcher.BeginInvoke(function, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[7], parameters[8], parameters[9]).Wait();
-                                break;
-                            default:
-                                throw new InvalidOperationException("Parameters are too much.");
-                        }
-                    }
-                },
-            null,
-            (int)(delayInSeconds * 1000),
-            Timeout.Infinite);
+            DelayActionParameters parameters = new DelayActionParameters(dispatcher, function, delayInSeconds, parametersList);
+            Thread thread = new Thread(new ParameterizedThreadStart(new ParameterizedThreadStart(CallFunctionThreadStart)));
+            thread.Start(parameters);
         }
 
         /// <summary>
@@ -383,7 +359,7 @@
         /// <param name="parameters">Removal parameters</param>
         private static void RemoveElementThreadStart(object parameters)
         {
-            RemoveElementParameters param = (RemoveElementParameters)parameters;
+            DelayActionParameters param = (DelayActionParameters)parameters;
             Thread.Sleep((int)(param.DelayInSeconds * 1000));
             if (param.Panel.Dispatcher.Thread == Thread.CurrentThread)
             {
@@ -392,6 +368,112 @@
             else
             {
                 param.Panel.Dispatcher.BeginInvoke(new RemoveElementDelegate(RemoveElementHelper), param.Element, param.Panel).Wait();
+            }
+        }
+
+        /// <summary>
+        /// Thread start for control hiding
+        /// </summary>
+        /// <param name="parameters">Removal parameters</param>
+        private static void HideElementThreadStart(object parameters)
+        {
+            DelayActionParameters param = (DelayActionParameters)parameters;
+            Thread.Sleep((int)(param.DelayInSeconds * 1000));
+            if (param.Element.Dispatcher.Thread == Thread.CurrentThread)
+            {
+                HideElementHelper(param.Element);
+            }
+            else
+            {
+                param.Element.Dispatcher.BeginInvoke(new HideElementDelegate(HideElementHelper), param.Element).Wait();
+            }
+        }
+
+        private static void CallFunctionThreadStart(object parameters)
+        {
+            DelayActionParameters param = (DelayActionParameters)parameters;
+            Thread.Sleep((int)(param.DelayInSeconds * 1000));
+            if (param.Dispatcher.Thread == Thread.CurrentThread)
+            {
+                switch (param.Parameters.Length)
+                {
+                    case 0:
+                        param.Function.DynamicInvoke();
+                        break;
+                    case 1:
+                        param.Function.DynamicInvoke(param.Parameters[0]);
+                        break;
+                    case 2:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1]);
+                        break;
+                    case 3:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2]);
+                        break;
+                    case 4:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3]);
+                        break;
+                    case 5:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4]);
+                        break;
+                    case 6:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5]);
+                        break;
+                    case 7:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6]);
+                        break;
+                    case 8:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[1]);
+                        break;
+                    case 9:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[7], param.Parameters[8]);
+                        break;
+                    case 10:
+                        param.Function.DynamicInvoke(param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[7], param.Parameters[8], param.Parameters[9]);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Parameters are too much.");
+                }
+            }
+            else
+            {
+                switch (param.Parameters.Length)
+                {
+                    case 0:
+                        param.Dispatcher.BeginInvoke(param.Function).Wait();
+                        break;
+                    case 1:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0]).Wait();
+                        break;
+                    case 2:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1]).Wait();
+                        break;
+                    case 3:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2]).Wait();
+                        break;
+                    case 4:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3]).Wait();
+                        break;
+                    case 5:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4]).Wait();
+                        break;
+                    case 6:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5]).Wait();
+                        break;
+                    case 7:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6]).Wait();
+                        break;
+                    case 8:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[1]).Wait();
+                        break;
+                    case 9:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[7], param.Parameters[8]).Wait();
+                        break;
+                    case 10:
+                        param.Dispatcher.BeginInvoke(param.Function, param.Parameters[0], param.Parameters[1], param.Parameters[2], param.Parameters[3], param.Parameters[4], param.Parameters[5], param.Parameters[6], param.Parameters[7], param.Parameters[8], param.Parameters[9]).Wait();
+                        break;
+                    default:
+                        throw new InvalidOperationException("Parameters are too much.");
+                }
             }
         }
 
